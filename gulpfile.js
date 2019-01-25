57c2a359f02887fa394e919ee89851354eaa729a
Naiji Zhang <naijizhang@gmail.com>
2019-01-25 11:46:44 -0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+const gulp = require("gulp"),
+  terser = require("gulp-terser"),
+  rename = require("gulp-rename"),
+  uglifycss = require("gulp-uglifycss"),
+  browserSync = require("browser-sync").create(),
+  eslint = require("gulp-eslint"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  prettyError = require("gulp-prettyerror");
+
+//Task to minify css and reload the page
+// gulp.task("css-minify-reload", function() {
+//   gulp
+//     .src("./css/*.css")
+//     .pipe(uglifycss())
+//     .pipe(rename({ extname: ".min.css" }))
+//     .pipe(gulp.dest("./build/css"));
+//   browserSync.reload();
+//   done();
+// });
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/*.scss")
+    .pipe(prettyError()) //????
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(uglifycss())
+    .pipe(rename({ extname: ".min.css" }))
+    .pipe(gulp.dest("./build/css"));
+});
+
+// //Task to watch for changes to css files and do minify
+// gulp.task("watch-css", function() {
+//   gulp.watch("css/*.css", gulp.series("css-minify-reload"));
+//   gulp.watch("js/*.js", gulp.series("lint-js","scripts-reload"));
+
+// });
+//Task to watch for changes to css files and do minify
+gulp.task("watch", function() {
+  gulp.watch("sass/*.scss", gulp.series("sass"));
+  gulp.watch("js/*.js", gulp.series("scripts"));
+  // gulp.watch("js/*.js", gulp.series("lint-js","scripts"));
+  //done();
+});
+
+//Load Browsersync
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("build/css/*.css").on("change", browserSync.reload);
+  //done();
+});
+
+//Default task
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
+
+gulp.task("scripts", function() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(terser())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest("./build/js"));
+});
+gulp.task("lint", function() {
+  return (
+    gulp
+      .src(["./js/*.js"])
+      // Also need to use it here...
+      .pipe(prettyError())
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError())
+  );
+});
+gulp.task("scripts",gulp.series("lint", function() {
+    return gulp
+      .src("./js/*.js")
+      .pipe(terser())
+      .pipe(rename({extname: ".min.js"}))
+      .pipe(gulp.dest("./build/js"));
+  })
+);
+
+gulp.task("lint-js", function() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(prettyError())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(function() {
+  $("#sections").on("change", function() {
+    var sectionName = $(this).val();
+
+    //if value=null  return
+    //  show loading
+    //clear stories
+    $('.placeHolder').empty();
+    //make ajax request
+    $.ajax({
+      method: "GET",
+      url:
+        "https://api.nytimes.com/svc/topstories/v2/" +
+        sectionName +
+        ".json?api-key=o64EGq5PWtGS9Lp9oAji4kjH9pA2sNdR",
+      dataType: "json"
+    })
+      .done(function(data) {
+        //append all the things
+        var index;
+        for (index in data.results.slice(0,12)) {
+        //   $(".sectionContent").append('<p>'+data.results[index].title+'</p>');
+          $(".sectionContent").append('<div class="item"><img src="'+data.results[index].multimedia[4].url+'"><p>'+data.results[index].abstract+'</p></div>');
+          
+        }
+      })
+      .fail(function() {
+        console.log("erroe");
+      })
+      .always(function() {
+        //hide loader
+      });
+  });
+});
+//api key
+//o64EGq5PWtGS9Lp9oAji4kjH9pA2sNdR
+
+//Problem : retrieve content from the NYT top stories API and add it our site
+
+//error response
+
+//1.listen for menu change
+
+//1b.show loading
+
+//2.send request to NYT base on menu change
+
+//3.if success, apend to DOM
+
+//4.if unsuccess, show helpful to user
+
+//5.hide the loader
